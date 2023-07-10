cca76224a0b1b2acdf10a5e3b52d9bd5f6fcc32d
Logan Dietel <logandietel1@gmail.com>
2023-07-10 14:49:33 -0700

    Add scores to UI and restart game feature

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btnRock = document.createElement('button');
 const btnPaper = document.createElement('button');
 const btnScissors = document.createElement('button');
 const parentElement = document.querySelector('.container');
+const gameInfoElement = document.querySelector('.game-info');
 
 btnRock.textContent = 'rock';
 parentElement.appendChild(btnRock);
@@ -16,6 +17,24 @@ btnRock.addEventListener("click", btnClick);
 btnPaper.addEventListener("click", btnClick);
 btnScissors.addEventListener("click", btnClick);
 
+let playerCount = 0;
+
+let computerCount = 0;
+
+const scores = document.querySelector('.scores')
+
+const playerScoreMsg = document.createElement('h2');
+const playerScoreSentence = "Your score is " + playerCount;
+playerScoreMsg.innerText = playerScoreSentence; 
+scores.appendChild(playerScoreMsg);
+
+const computerScoreMsg = document.createElement('h2');
+const computerScoreSentence = "Computer score is " + computerCount;
+computerScoreMsg.innerText = computerScoreSentence; 
+scores.appendChild(computerScoreMsg);
+
+
+
 let btnClickResult;
 let roundCount = 0;
 
@@ -24,13 +43,13 @@ function btnClick(e) {
     btnClickResult = e.target.textContent;
     playRound(btnClickResult, computerSelection());
     roundCount++;
+    playerScoreMsg.innerText = "Your score is " + playerCount;
+    computerScoreMsg.innerText = "Computer score is " +computerCount;
     }
 
     if (roundCount === 5) {
         return finalScore(playerCount, computerCount);
     }
-
-    
 }
 
 let computerGuess;
@@ -55,6 +74,9 @@ function playRound(playerGuess, computerGuess) {
     if (
         playerGuess === computerGuess
     ) {
+        const tieMsg = document.createElement('p');
+        tieMsg.textContent = "It's a tie!";
+        gameInfoElement.appendChild(tieMsg);
         console.log("It's a tie!");
 
     } else if (
@@ -62,24 +84,75 @@ function playRound(playerGuess, computerGuess) {
         playerGuess === "paper" && computerGuess === "rock" ||
         playerGuess === "scissors" && computerGuess === "paper"
     ) {
+        const playerWinMsg = document.createElement('p');
+        playerWinMsg.textContent = "You Won!";
+        gameInfoElement.appendChild(playerWinMsg);
         console.log("You Won!");
         return ++playerCount;
     } else {
+        const computerWinMsg = document.createElement('p');
+        computerWinMsg.textContent = "Computer Won!";
+        gameInfoElement.appendChild(computerWinMsg);
         console.log("Computer Won!");
         return ++computerCount;
     }
 }
 
-let playerCount = 0;
+let gameInProgress = true; // Flag to track game state
 
-let computerCount = 0;
+// ...
+
+function restartGame() {
+  if (gameInProgress) {
+    return; // Only restart the game if it's already over
+  }
+
+  // Reset the player count, computer count, and round count
+  playerCount = 0;
+  computerCount = 0;
+  roundCount = 0;
+
+  // Clear game information displayed on the webpage
+  gameInfoElement.innerHTML = '';
+
+  // Update player score display
+  playerScoreMsg.innerText = "Your score is " + playerCount;
+
+  // Update computer score display
+  computerScoreMsg.innerText = "Computer score is " + computerCount;
+
+  // Reset the game state flag
+  gameInProgress = true;
+
+  // Add event listener to the buttons again
+  btnRock.addEventListener("click", btnClick);
+  btnPaper.addEventListener("click", btnClick);
+  btnScissors.addEventListener("click", btnClick);
+}
+
 
 function finalScore(playerCount, computerCount) {
     if (playerCount === computerCount) {
+        const tieGameMsg = document.createElement('h1');
+        tieGameMsg.textContent = "The final score of the game is a tie!";
+        gameInfoElement.appendChild(tieGameMsg);
         console.log("The final score of the game is a tie!");
     } else if (playerCount > computerCount) {
+        const playerWinGameMsg = document.createElement('h1');
+        playerWinGameMsg.textContent = "You Won the Game!";
+        gameInfoElement.appendChild(playerWinGameMsg);
         console.log("You won the game!");
     } else {
+        const computerWinGameMsg = document.createElement('h1');
+        computerWinGameMsg.textContent = "You lost the game!";
+        gameInfoElement.appendChild(computerWinGameMsg);
         console.log("You lost the game!");
     }
+
+    gameInProgress = false;
+    const finalScoreMsg = document.createElement('p');
+    finalScoreMsg.textContent = "Click here to restart the game";
+    finalScoreMsg.classList.add('restart-game');
+    finalScoreMsg.addEventListener('click', restartGame);
+    gameInfoElement.appendChild(finalScoreMsg);
 }
